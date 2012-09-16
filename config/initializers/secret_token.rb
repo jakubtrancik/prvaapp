4b1c5b3fbfebfd97349b00fd1dc32275a4973dcf
Jakub Trancik <jakub.trancik@gmail.com>
2012-09-16 20:48:01 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prvaapp::Application.config.secret_token = '165da8464429e95dcd6bbf9bac505463a90a6ca5d6931eba7ab1d5e4657145e3b3999e54ac26379c0f177c22240fca799dfbce20d64e966302255ddfd892c5e7'
